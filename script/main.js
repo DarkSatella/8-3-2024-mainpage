aa9d0cdc1085516ae4b31712c098a69240bfccab
DarkSatella <konu0315@gmail.com>
2024-03-06 08:53:32 +0700

    first commit

---
+++ script/main.js
@@ -0,0 +1,8 @@
+const myButton = document.getElementById('myButton');
+
+
+
+myButton.addEventListener('click', async () => { // Use async for Promise handling
+  
+  myButton.classList.add('fade-out');
+});
